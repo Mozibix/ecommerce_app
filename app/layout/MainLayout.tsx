c492615ff27d27c1f8f42d13e3547614b033d7ec
Mozibix <opuzmoses@gmail.com>
2023-09-06 07:28:37 +0100

    chores updated mainlayout component

--- app/layout/MainLayout.tsx
+++ app/layout/MainLayout.tsx
@@ -1,15 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Footer from "./includes/Footer";
 import MainHeader from "./includes/MainHeader";
 import SubMenu from "./includes/SubMenu";
 import TopMenu from "./includes/TopMenu";
+import Loading from "../(components)/Loading";
 
 interface IProps {
   children: any;
 }
 
 const MainLayout: React.FC<IProps> = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("storage", function () {
+      let res = localStorage.getItem("isLoading");
+      res === "false" ? setIsLoading(false) : setIsLoading(true);
+    });
+  });
+
   return (
     <>
       <div
@@ -17,6 +28,8 @@ const MainLayout: React.FC<IProps> = ({ children }) => {
         className="min-w-[1050px] max-w-[1300px] mx-auto border"
       >
         <div>
+          {/* LOADING */}
+          {isLoading ? <Loading /> : <></>}
           {/* TOPMENU */}
           <TopMenu />
           {/* MAINHEADER */}
