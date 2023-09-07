411e0dff97859b23429cd772c9e4aab7f2c04396
Mozibix <opuzmoses@gmail.com>
2023-09-08 00:24:07 +0100

    chores: updated MainHeader component

--- app/layout/includes/MainHeader.tsx
+++ app/layout/includes/MainHeader.tsx
@@ -1,17 +1,60 @@
 "use client";
 
+import { debounce } from "debounce";
 import Link from "next/link";
+import { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { BiLoaderCircle } from "react-icons/bi";
 
-export default function MainHeader() {
+interface Product {
+  id: number;
+  title: string;
+  url: string;
+  price: number;
+}
+
+export default function MainLayout() {
+  const [items, setItems] = useState<Product[]>([]);
+  const [isSearching, setIsSearching] = useState<boolean | null>(null);
+
+  const handleSearchName = debounce(
+    async (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (event.target.value === "") {
+        setItems([]);
+        return;
+      }
+
+      setIsSearching(true);
+
+      try {
+        const response = await fetch(
+          `/api/products/search_by_name/${event.target.value}`
+        );
+        const result: Product[] = await response.json();
+
+        if (result) {
+          setItems(result);
+          setIsSearching(false);
+          return;
+        }
+        setItems([]);
+        setIsSearching(false);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    },
+    500
+  );
+
   return (
     <>
-      <div id="main_header" className="border-b">
-        <div className="flex items-center justify-between w-full mx-auto max-w-[1200px]">
+      <div id="MainHeader" className="border-b">
+        <nav className="flex items-center justify-between w-full mx-auto max-w-[1200px]">
           <div className="flex items-center w-full bg-white">
-            <div className="flex lg:justify-start justify-between gap-10 px-3 max-w-[1150px] w-full">
+            <div className="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto">
               <Link href="/">
-                <img width={120} src="/images/logo.svg" alt="logo" />
+                <img width="120" src="/images/logo.svg" alt="Logo" />
               </Link>
 
               <div className="w-full">
@@ -23,14 +66,56 @@ export default function MainHeader() {
                       </button>
 
                       <input
+                        className="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                        onChange={handleSearchName}
+                        placeholder="Search for anything"
                         type="text"
-                        placeholder="search for anything"
-                        className="w-full placeholder-gray-400 texy-sm pl-3 focus:outline-none"
                       />
+
+                      {isSearching ? (
+                        <BiLoaderCircle
+                          className="mr-2 animate-spin"
+                          size={22}
+                        />
+                      ) : (
+                        ""
+                      )}
+
+                      {items.length > 0 ? (
+                        <>
+                          <div className="absolute bg-white max-w-[910px] h-auto w-full z-20 left-0 top-12 border p-1">
+                            {items.map((item) => (
+                              <div className="p-1" key={item.id}>
+                                <Link
+                                  href={`/product/${item?.id}`}
+                                  className="flex items-center justify-between w-full cursor-pointer hover:bg-gray-200 p-1 px-2"
+                                >
+                                  <div className="flex items-center">
+                                    <img
+                                      className="rounded-md"
+                                      width="40"
+                                      src={item.url + "/40"}
+                                      alt={item.title}
+                                    />
+                                    <div className="truncate ml-2">
+                                      {item.title}
+                                    </div>
+                                  </div>
+                                  <div className="truncate">
+                                    ${(item.price / 100).toFixed(2)}
+                                  </div>
+                                </Link>
+                              </div>
+                            ))}
+                          </div>
+                        </>
+                      ) : (
+                        ""
+                      )}
                     </div>
 
                     <button className="flex items-center bg-blue-600 text-sm font-semibold text-white p-[11px] ml-2 px-14">
-                      search
+                      Search
                     </button>
 
                     <div className="text-xs px-2 hover:text-blue-500 cursor-pointer">
@@ -41,7 +126,7 @@ export default function MainHeader() {
               </div>
             </div>
           </div>
-        </div>
+        </nav>
       </div>
     </>
   );
