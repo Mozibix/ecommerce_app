a9cad81d26fef326d68c84a76433e25e9a9a0fcb
Mozibix <opuzmoses@gmail.com>
2023-09-09 23:27:02 +0100

    chores: updating seed.ts data component

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,7 +9,7 @@ async function seedProducts() {
         description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         url: "https://picsum.photos/id/7",
-        price: 2500, // EG: 25.00
+        price: 2500,
       },
     });
 
@@ -200,6 +200,22 @@ async function seedProducts() {
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         url: "https://picsum.photos/id/145",
         price: 12595,
+      },
+    });
+    await prisma.products.create({
+      data: {
+        title: "Gp products",
+        description: "a product for testing",
+        url: "/images/banner/1.png",
+        price: 99999,
+      },
+    });
+    await prisma.products.create({
+      data: {
+        title: "Nation Product",
+        description: "a good product that is for nation",
+        url: "/images/banner/2.png",
+        price: 55555,
       },
     });
   } catch (error) {
